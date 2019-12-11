042279d2c7d9c68000c7044488199ed371a8f686
Jacob Feldman <me@jacobfeldman.dev>
2019-12-11 10:16:29 -0500

    Completes day_6 part 2

--- day_6/day_6.rb
+++ day_6/day_6.rb
@@ -1,41 +1,90 @@
 require './day_6/input.rb'
 require './day_6/test_input.rb'
 
-class Planet
-  attr_reader :name, :satellites
+class Satellite
+  attr_reader :name, :satellites, :orbiting
   def initialize(name)
     @name = name
     @satellites = []
+    @orbiting = nil
   end
 
   def add_satellite(satellite)
     @satellites << satellite
   end
+
+  def add_orbiting(satellite)
+    @orbiting = satellite
+  end
+
+  def distance_to_inner_satellite(inner)
+    total = 0
+    current = self
+    while current != inner
+      current = current.orbiting
+      total += 1
+    end
+
+    total
+  end
+
+  def distance_to_satellite(satellite)
+    inner = find_common_inner_orbit(satellite)
+
+    distance_to_inner_satellite(inner) +
+    satellite.distance_to_inner_satellite(inner)
+  end
+
+  def find_common_inner_orbit(satellite)
+    inner = self
+    to_check = inner.satellites.dup
+
+    while inner
+      while to_check.any?
+        sat = to_check.pop
+        return inner if sat == satellite
+
+        to_check += sat.satellites
+      end
+
+      inner = inner.orbiting
+      to_check += inner.satellites
+    end
+  end
 end
 
 class System
-  attr_reader :satellites
+  attr_reader :satellites, :root
 
   def initialize(orbits)
     @satellites = {}
-    @root = orbits.first.split(')').first
 
     orbits.each do |orbit|
       inner, outer = orbit.split(')')
 
-      inner = @satellites[inner] || (@satellites[inner] = Planet.new(inner))
-      outer = @satellites[outer] || (@satellites[outer] = Planet.new(outer))
+      inner = @satellites[inner] || (@satellites[inner] = Satellite.new(inner))
+      outer = @satellites[outer] || (@satellites[outer] = Satellite.new(outer))
 
       inner.add_satellite(outer)
+      outer.add_orbiting(inner)
     end
+
+    set_root
   end
 
-  def orbit_count(node = satellites[@root], previous_total = 0)
+  def set_root
+    satellite = @satellites.first.last
+    satellite = satellite.orbiting while satellite.orbiting
+
+    @root = satellite
+  end
+
+  def orbit_count(node = @root, distance = 0)
     return 0 unless node.satellites
 
-    total = previous_total
+    total = distance
     node.satellites.each do |satellite|
-      total += orbit_count(node = satellite, previous_total + 1)
+      total += orbit_count(node = satellite, distance + 1)
     end
 
     total
@@ -44,6 +93,9 @@ end
 
 test_system = System.new(TEST_ORBITS)
 system = System.new(ORBITS)
+test = test_system.satellites['I'].find_common_inner_orbit(test_system.satellites['zeta']).name
+test2 = test_system.satellites['I'].distance_to_satellite(test_system.satellites['zeta'])
+you_to_san = system.satellites['YOU'].distance_to_satellite(system.satellites['SAN']) - 2
 
 require 'pry'
 binding.pry
--- day_6/test_input.rb
+++ day_6/test_input.rb
@@ -7,6 +7,8 @@ TEST_ORBITS = [
   'B)G',
   'G)H',
   'D)I',
+  'D)z',
+  'z)zeta',
   'E)J',
   'J)K',
   'K)L'
